7b72499b8f75f4924c1502b2e226f389cad0b0fb
Chadwick Koo <chadwickkoo@outlook.com>
2025-05-04 11:19:14 +0300

    Modified files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <div>Your code here</div>;
+  return(
+    <div>
+      <h1>Hi, i'm Chad</h1>
+      <img src="./images/bball.jpeg" alt="Profile Picture"/>
+      <h2>About Me</h2>
+      <p>Bio</p>
+      <a href="https://github.com/ChadKoo">Github Profile</a>
+      <a href='https://www.linkedin.com/in/chadwick-koo-281b92363'>LinkedIn</a>
+    </div>);
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,60 @@
-import { render, screen } from "@testing-library/react";
+import {  render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom';
 
 import App from "../App";
 
-// Your tests here
+test("renders github  and linkedin links", () => {
+    render(<App />);
+    
+
+   
+  
+    const githubLink = screen.getByRole("link", {name: /Github Profile/i});
+    expect(githubLink).toBeInTheDocument();
+    expect(githubLink).toHaveAttribute("href", "https://github.com/ChadKoo");
+
+    const linkedin = screen.getByRole("link", {name: /LinkedIn/i});
+    expect(linkedin).toBeInTheDocument();
+    expect(linkedin).toHaveAttribute("href", "https://www.linkedin.com/in/chadwick-koo-281b92363");
+
+  });
+
+  test("displays a top level heading with the text 'Hi, I'm Chad'", () => {
+    render(<App/>);
+
+    const topLevelHeading = screen.getByRole("heading",{
+      name: /hi, I'm Chad/i,
+      exact: false,
+      level: 1,
+  })
+  expect(topLevelHeading).toBeInTheDocument();
+})
+
+
+test("displays a 2nd level heading with the text 'About Me'", () => {
+  render(<App/>);
+
+  const secondLevelHeading = screen.getByRole("heading",{
+    name: /About me/i,
+    exact: false,
+    level: 2,
+})
+expect(secondLevelHeading).toBeInTheDocument();
+})
+
+test("displays a paragraph with the text 'bio'", () => {
+  render(<App/>);
+
+  const paragraph = screen.getByText("Bio",{exact: false})
+
+expect(paragraph).toBeInTheDocument();
+})
+
+test("displays an image with the alttext  of 'profile picture'", () => {
+  render(<App/>);
+
+  const image = screen.getByRole("img",{exact: false})
+
+expect(image).toBeInTheDocument();
+expect(image).toHaveAttribute("src", "./images/bball.jpeg")
+})
